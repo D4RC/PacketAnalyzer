8835281c7068508756b9922fef83c84dfc1c3075
DARC <darc3012@gmail.com>
2020-10-31 20:47:37 -0600

    feat: vector data struct

---
+++ vector.h
@@ -0,0 +1,16 @@
+#ifndef VECTOR_H__
+#define VECTOR_H__
+
+typedef struct vector_ {
+    void** data;
+    int size;
+    int count;
+} vector;
+
+void vector_init(vector*);
+int vector_count(vector*);
+void vector_add(vector*, void*);
+void *vector_get(vector*, int);
+void vector_free(vector*);
+
+#endif
